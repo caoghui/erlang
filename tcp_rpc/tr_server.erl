811a9b058db53c2019d6d7a4450396ec193d6a31
caogh1 <caogh1@lenovo.com>
2017-09-01 18:05:29 +0800

    add internal functions

--- tcp_rpc/tr_server.erl
+++ tcp_rpc/tr_server.erl
@@ -56,8 +56,58 @@ get_count() ->
 stop() ->
     gen_server:cast(?SERVER, stop).
 
+%%%-----------------------------------------------%%%
+%%%--------------gen_server callback 段-----------%%%
+%%%-----------------------------------------------%%%
+init([Port]) ->
+    {ok, LSock} = gen_tcp:listen(Port, [{active, true}]),
+    {ok, #state{port = Port, lsock = LSock}, 0}.
+
+handle_call(get_count, _Form, State) ->
+    {reply, {ok, State#state.request_count}, State}.
+
+handle_cast(stop, State) ->
+    {stop, normal, State}.
 
 
+handle_info({tcp, Socket, RawData}, State) ->
+    do_rpc(Socket, RawData),
+    RequestCount = State#state.request_count,
+    {noreply, State#state{request_count = RequestCount + 1}};
+handle_info(timeout, #state{lsock = LSock} = State) ->
+    {ok, _Sock} = gen_tcp:accept(LSock),
+    {noreply, State}.
+
+terminate(_Reason, _State) -> 
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%-----------------------------------------------%%%
+%%%--------------Internal functions-----------%%%
+%%%-----------------------------------------------%%%
+do_rpc(Socket, RawData) ->
+    try
+        {M, F, A} = split_out_mfa(RawData),
+        Result = apply(M, F, A),
+        gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Result]))
+    catch
+        _Class:Err ->
+            gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Err]))
+    end.
+
+split_out_mfa(RawData) ->
+    MFA = re:replace(RawData, "\r\n$", "", [{return, list}]),
+    {match, [M, F, A]} = re:run(MFA, 
+                                "(.*):(.*)\s*\\((.*)\s*\\)\s*.\s*$", 
+                                [{capture, [1, 2, 3], ungreedy}]),
+    {list_to_atom(M), list_to_atom(F), args_to_terms(A)}.
+
+args_to_terms(RawArgs) ->
+    {ok, Toks, _Line} = erl_scan:string("[" ++ RawArgs ++ "]. ", 1),
+    {ok, Args} = erl_parse:parse_term(Toks),
+    Args.
 
 %%% 测试函数
 start() ->
